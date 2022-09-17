42f4b3cb824444d3e9adb1414778f5486175b232
waghdipak375 <waghdipak375@gmail.com>
2022-09-17 16:23:23 +0530

    Custom User Model, User Profile, Media Files, Signals

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,11 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     BASE_DIR/'foodOnline_main'/'static'
 ]
-STATIC_ROOT = BASE_DIR/'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 
 # Default primary key field type
